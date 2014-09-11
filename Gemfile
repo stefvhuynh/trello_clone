7fe783ff9899e74400613c6f61a254c0593624b2
Stefan Huynh <huynh.stefan@gmail.com>
2014-09-10 22:33:53 -0400

    Add backbone and remove coffeescript files

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'bcrypt', '~> 3.1.7'
 gem 'debugger', group: [:development, :test]
+gem 'backbone-on-rails'
 
 group :development do
   gem 'pry-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   
-  get 'root', as: 'pages#root'
+  root to: 'pages#root'
   
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:index, :update]
