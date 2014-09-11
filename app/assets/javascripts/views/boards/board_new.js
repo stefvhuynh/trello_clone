c902771dff0d64207e18e1e9bcf1d4f90a15a2c6
Stefan Huynh <huynh.stefan@gmail.com>
2014-09-11 15:41:15 -0400

    Start board new submit function

--- app/assets/javascripts/views/boards/board_new.js
+++ app/assets/javascripts/views/boards/board_new.js
@@ -3,12 +3,18 @@ TrelloClone.Views.BoardNew = Backbone.View.extend({
   className: 'board-new',
 
   events: {
-
+    'submit .board-new-form': 'submit'
   },
 
   render: function() {
     var content = this.template();
     this.$el.html(content);
     return this;
+  },
+
+  submit: function(event) {
+    event.preventDefault();
+    var boardData = $(event.currentTarget).serializeJSON().board;
+
   }
 });
