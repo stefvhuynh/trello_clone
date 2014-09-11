9f5ccf3715f1938bef79d5d41f376449b7f10a1f
Stefan Huynh <huynh.stefan@gmail.com>
2014-09-11 08:46:14 -0400

    Minor changes

--- app/assets/javascripts/views/boards_index.js
+++ app/assets/javascripts/views/boards_index.js
@@ -1,6 +1,10 @@
 TrelloClone.Views.BoardsIndex = Backbone.View.extend({
   template: JST['boards/index'],
   
+  initialize: function() {
+    
+  };
+  
   render: function() {
     var content = this.template({ boards: this.collection });
     this.$el.html(content);
