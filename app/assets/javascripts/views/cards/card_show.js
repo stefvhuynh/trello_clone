7ad087fd81f8e6218709898c9f16992ce7b4f8d7
Stefan Huynh <huynh.stefan@gmail.com>
2014-09-13 13:05:53 -0400

    Add card show view

---
+++ app/assets/javascripts/views/cards/card_show.js
@@ -0,0 +1,10 @@
+TrelloClone.Views.CardShow = Backbone.View.extend({
+  template: JST['cards/show'],
+  tagName: 'li',
+  
+  render: function() {
+    var content = this.template({ card: this.model });
+    this.$el.html(content);
+    return this;
+  }
+});
